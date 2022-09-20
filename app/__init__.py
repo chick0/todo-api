2305efe7199b4d93ad5be8a39efa9c12489440c3
chick_0 <chick_0@ch1ck.xyz>
2022-09-20 12:51:33 +0900

    chore: Setup SQLAlchemy for control database
    And alembic for manage database models

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,21 @@
+from os import environ
+from importlib import import_module
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ['SQLALCHEMY_DATABASE_URI']
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    import_module("app.models")
+    db.init_app(app=app)
+    migrate.init_app(app=app, db=db)
+
     return app
---
+++ app/models.py
@@ -0,0 +1,65 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = "td_user"
+
+    id = db.Column(
+        db.Integer,
+        unique=True,
+        primary_key=True,
+        nullable=False
+    )
+
+    email = db.Column(
+        db.String(120),
+        unique=True,
+        nullable=False
+    )
+
+    password = db.Column(
+        db.String(128),
+        nullable=False
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+    )
+
+    lastlogin = db.Column(
+        db.DateTime,
+        nullable=False,
+    )
+
+
+class Todo(db.Model):
+    __tablename__ = "td_todo"
+
+    id = db.Column(
+        db.Integer,
+        unique=True,
+        primary_key=True,
+        nullable=False
+    )
+
+    owner = db.Column(
+        db.Integer,
+        db.ForeignKey("td_user.id"),
+        nullable=False
+    )
+
+    checked = db.Column(
+        db.Boolean,
+        nullable=False,
+    )
+
+    text = db.Column(
+        db.Text(500),
+        nullable=False
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+    )
