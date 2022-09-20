1952e1824dc4d66f00c6d62dffe66d21f38f1087
chick_0 <chick_0@ch1ck.xyz>
2022-09-20 14:55:50 +0900

    feat: Send from dist

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from os import environ
+from os.path import join
+from os.path import abspath
+from os.path import dirname
 from importlib import import_module
 
 from flask import Flask
+from flask import Response
+from flask import send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -17,5 +22,31 @@ def create_app():
     import_module("app.models")
     db.init_app(app=app)
     migrate.init_app(app=app, db=db)
+
+    BASE_DIR = dirname(dirname(abspath(__file__)))
+    DIST_DIR = join(BASE_DIR, "dist")
+
+    @app.get("/")
+    @app.get("/<path:path>")
+    def frontend(path = None):
+        if path is None:
+            path = "index.html"
+
+        response: Response = send_from_directory(
+            directory=DIST_DIR,
+            path=path
+        )
+
+        if path.endswith(".js"):
+            response.content_type = "text/javascript; charset=utf-8"
+
+        return response
+
+
+    @app.get("/api/sudo")
+    def sudo():
+        return {
+            "sudo": False
+        }, 404
 
     return app
