ee7316fd83d7afa42a5b787bb557bddd01c14a42
chick_0 <chick_0@ch1ck.xyz>
2022-09-21 10:41:41 +0900

    feat: Add logout and quit button in about page

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,8 +1,18 @@
+<script>
+    import { is_login } from "../user.js";
+</script>
+
 <div class="section container">
     <h1>To-Do</h1>
     <div class="buttons">
-        <a class="button" href="#/login">로그인</a>
-        <a class="button" href="#/sign-up">회원가입</a>
+        {#if !is_login()}
+            <a class="button" href="#/login">로그인</a>
+            <a class="button" href="#/sign-up">회원가입</a>
+        {:else}
+            <a class="button" href="#/todo">할 일</a>
+            <a class="button" href="#/logout">로그아웃</a>
+            <a class="button" href="#/quit">서비스 탈퇴</a>
+        {/if}
     </div>
 
     <hr />
