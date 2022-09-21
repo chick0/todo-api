be21116fb73ac34409ded0f167ce4b0047ddc919
chick_0 <chick_0@ch1ck.xyz>
2022-09-21 12:36:07 +0900

    feat: Create logout api
    Remove 'DBSession' on request

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,6 @@
 __all__ = [
     "login",
+    "logout",
     "sign-up",
     "todo",
     "verify",
---
+++ app/routes/logout.py
@@ -0,0 +1,29 @@
+from flask import Blueprint
+from pydantic import BaseModel
+
+from app import db
+from app.models import DBSession
+from app.auth import AuthSession
+from app.auth import login_required
+
+bp = Blueprint("logout", __name__, url_prefix="/api/logout")
+
+
+class LogoutResponse(BaseModel):
+    result: bool
+
+
+@bp.delete("")
+@login_required
+def logout(session: AuthSession):
+    dbs = DBSession.query.filter_by(
+        id=session.sid,
+        owner=session.user_id
+    ).delete()
+
+    if dbs != 0:
+        db.session.commit()
+
+    return LogoutResponse(
+        result=True
+    ).dict()
