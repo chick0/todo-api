19b1a11420f9637a202b1e18a171d9af05c7ee32
chick_0 <chick_0@ch1ck.xyz>
2022-09-22 14:18:30 +0900

    chore: Change 'NotFound' to normal
    Not asyncComponent

--- src/router.js
+++ src/router.js
@@ -3,30 +3,19 @@ import { wrap } from "svelte-spa-router/wrap";
 import About from "./routes/About.svelte";
 import Login from "./routes/Login.svelte";
 import Logout from "./routes/Logout.svelte";
+import NotFound from "./NotFound.svelte";
 
 export const routes = {
     "/": About,
     "/login": Login,
     "/logout": Logout,
-    "/sign-up": wrap({
-        asyncComponent: () => import("./routes/SignUp.svelte"),
-    }),
-    "/verify": wrap({
-        asyncComponent: () => import("./routes/Verify.svelte"),
-    }),
+    "/sign-up": wrap({ asyncComponent: () => import("./routes/SignUp.svelte") }),
+    "/verify": wrap({ asyncComponent: () => import("./routes/Verify.svelte") }),
 
-    "/todo": wrap({
-        asyncComponent: () => import("./routes/Todo.svelte"),
-    }),
+    "/todo": wrap({ asyncComponent: () => import("./routes/Todo.svelte") }),
 
-    "/user": wrap({
-        asyncComponent: () => import("./routes/User.svelte"),
-    }),
-    "/user/quit": wrap({
-        asyncComponent: () => import("./routes/Quit.svelte"),
-    }),
+    "/user": wrap({ asyncComponent: () => import("./routes/User.svelte") }),
+    "/user/quit": wrap({ asyncComponent: () => import("./routes/Quit.svelte") }),
 
-    "*": wrap({
-        asyncComponent: () => import("./NotFound.svelte"),
-    }),
+    "*": NotFound,
 };
