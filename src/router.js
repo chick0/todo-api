66dd1e8ffc72e83b846ec55125cf56a357ba6451
chick_0 <chick_0@ch1ck.xyz>
2022-09-23 19:38:30 +0900

    chore: Change all component to async component

--- src/router.js
+++ src/router.js
@@ -1,14 +1,9 @@
 import { wrap } from "svelte-spa-router/wrap";
 
-import About from "./routes/About.svelte";
-import Login from "./routes/Login.svelte";
-import Logout from "./routes/Logout.svelte";
-import NotFound from "./NotFound.svelte";
-
 export const routes = {
-    "/": About,
-    "/login": Login,
-    "/logout": Logout,
+    "/": wrap({ asyncComponent: () => import("./routes/About.svelte") }),
+    "/login": wrap({ asyncComponent: () => import("./routes/Login.svelte") }),
+    "/logout": wrap({ asyncComponent: () => import("./routes/Logout.svelte") }),
     "/sign-up": wrap({ asyncComponent: () => import("./routes/SignUp.svelte") }),
     "/verify": wrap({ asyncComponent: () => import("./routes/Verify.svelte") }),
     "/retry": wrap({ asyncComponent: () => import("./routes/Retry.svelte") }),
@@ -22,5 +17,5 @@ export const routes = {
     "/pin": wrap({ asyncComponent: () => import("./routes/Pin.svelte") }),
     "/pin/create": wrap({ asyncComponent: () => import("./routes/PinCreate.svelte") }),
 
-    "*": NotFound,
+    "*": wrap({ asyncComponent: () => import("./NotFound.svelte") }),
 };
