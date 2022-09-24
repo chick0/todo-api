a92e0682134c6941ca7b39c6ce6cc85265d5a2cb
chick_0 <chick_0@ch1ck.xyz>
2022-09-24 13:20:17 +0900

    fix: Remove 'vite-plugin-pwa' plugin and create sw.js
    When an API request is generated, it will be cached.
    However, POST requests cannot be cached. So a critical error has occurred.
    So I removed plugin and create service worker manually.

---
+++ public/sw.js
@@ -0,0 +1,59 @@
+const RUNTIME = "runtime-v1";
+const PRECACHE = "precache";
+const PRECACHE_URLS = [
+    "icons/192.png",
+    "Pretendard/ExtraLight.woff2",
+    "Pretendard/Light.woff2",
+    "Pretendard/Regular.woff2",
+    "Pretendard/SemiBold.woff2,",
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches
+            .open(PRECACHE)
+            .then((cache) => cache.addAll(PRECACHE_URLS))
+            .then(self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches
+            .keys()
+            .then((cache_keys) => {
+                return cache_keys.filter((key) => !currentCaches.includes(key));
+            })
+            .then((delete_keys) => {
+                return Promise.all(
+                    delete_keys.map((key) => {
+                        return caches.delete(key);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.startsWith(self.location.origin) && !event.request.url.includes("api")) {
+        event.respondWith(
+            caches.match(event.request).then((cachedResponse) => {
+                console.log(event.request.url);
+
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(RUNTIME).then((cache) => {
+                    return fetch(event.request).then((response) => {
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
+});
