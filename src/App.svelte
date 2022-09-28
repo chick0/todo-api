567e3c4f89822d673c6366bc8379953f2c6cdea5
chick_0 <chick_0@ch1ck.xyz>
2022-09-28 19:48:59 +0900

    feat: Set client title

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
     import Router from "svelte-spa-router";
+    import { location } from "svelte-spa-router";
     import { onMount } from "svelte";
+    import { titles } from "./title.js";
     import { routes } from "./router.js";
 
     function update_theme_color() {
@@ -17,8 +19,19 @@
         window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => update_theme_color());
         window.onunhandledrejection = () => {
             alert("치명적인 오류가 발생해 재시작합니다.");
-            location.reload();
+            window.location.reload();
         };
+    });
+
+    location.subscribe((path) => {
+        let title = titles[path];
+
+        if (title == null) {
+            console.warn(`'${path}'에 제목이 설정되지 않았습니다.`);
+            title = "To-Do";
+        }
+
+        document.title = title;
     });
 </script>
 
---
+++ src/title.js
@@ -0,0 +1,21 @@
+export const titles = {
+    "/": "To-Do",
+    "/login": "로그인",
+    "/logout": "로그아웃",
+
+    "/sign-up": "회원가입",
+    "/verify": "이메일 인증",
+
+    "/retry": "이메일 인증",
+    "/reset": "비밀번호 재설정",
+
+    "/todo": "할 일",
+
+    "/user": "계정 정보",
+    "/user/quit": "회원 탈퇴",
+
+    "/pin": "PIN 로그인",
+    "/pin/create": "PIN 설정",
+
+    "/version": "버전 정보",
+};
