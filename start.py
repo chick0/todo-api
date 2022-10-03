14679786bf3e587fc05ccde36ff3e66c6143e3eb
chick_0 <chick_0@ch1ck.xyz>
2022-10-04 01:51:50 +0900

    chore: Use stdout in logging

--- start.py
+++ start.py
@@ -1,9 +1,12 @@
+from sys import stdout
 from logging import getLogger
 from logging import StreamHandler
 from logging import Formatter
 from logging import INFO
+
 from waitress import serve
 from dotenv import load_dotenv
+
 from app import create_app
 
 logger = getLogger()
@@ -11,7 +14,7 @@ logger = getLogger()
 
 def init_logger():
     logger.setLevel(INFO)
-    handler = StreamHandler()
+    handler = StreamHandler(stdout)
     handler.setFormatter(fmt=Formatter("%(asctime)s [%(levelname)s]: %(message)s", "%Y-%m-%d %H:%M:%S"))
     logger.addHandler(hdlr=handler)
 
@@ -27,7 +30,7 @@ def get_app():
     return app
 
 
-def main():
+def start_app():
     host, port = "127.0.0.1", 18282
     serve(app=get_app(), host=host, port=port)
 
@@ -35,4 +38,4 @@ def main():
 if __name__ == "__main__":
     load_dotenv()
     init_logger()
-    main()
+    start_app()
