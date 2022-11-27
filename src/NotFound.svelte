b53c5d5a6c72c1d5be96a29a14f617b8f47fbc2f
chick_0 <chick_0@ch1ck.xyz>
2022-11-27 18:11:35 +0900

    chore: Remove client files

--- src/NotFound.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    export let params = {};
-    import { push } from "svelte-spa-router";
-    push("/");
-</script>
--- src/vite-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="svelte" />
-/// <reference types="vite/client" />
-
-declare const TAG: string;
-declare const BUILD_DATE: number;
